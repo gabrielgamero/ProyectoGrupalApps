9bd5df0bb79fb2661e254f85bcaafe1e0cabbf38
juliorod <julio.rodriguezo@pucp.pe>
2020-06-30 13:43:30 -0500

    Registrar incompleto

--- app/src/main/java/pucp/telecom/moviles/incidencias/activities/RegisterActivity.java
+++ app/src/main/java/pucp/telecom/moviles/incidencias/activities/RegisterActivity.java
@@ -1,44 +1,43 @@
 package pucp.telecom.moviles.incidencias.activities;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import pucp.telecom.moviles.incidencias.CallbackInterface;
 import pucp.telecom.moviles.incidencias.R;
 import pucp.telecom.moviles.incidencias.entities.DtoMessage;
 import pucp.telecom.moviles.incidencias.webRequests.FireUser;
 
-public class LoginActivity extends AppCompatActivity {
-    // Login fields
+public class RegisterActivity extends AppCompatActivity {
+    // Register fields
     EditText email;
+    EditText code;
     EditText password;
-    // Login button
-    Button btnLogin;
+    // Register button
+    Button btnRegister;
 
     // Firebase Authentication
     FireUser fireUser;
-
     // Dtos
     DtoMessage dtoMessage;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); // activityLoginActivity
+        setContentView(R.layout.activity_register);
     }
 
-    public void doLogin(View view) {
-        String email = ((EditText) findViewById(R.id.editTextEmailPucpLogin)).getText().toString();
-        String password = ((EditText) findViewById(R.id.editTextPasswordLogin)).getText().toString();
+    public void doRegister() {
+        String email = ((EditText) findViewById(R.id.editTextEmailPucpRegister)).getText().toString();
+        String pucpCode = ((EditText) findViewById(R.id.editTextCodePucpRegister)).getText().toString();
+        String password = ((EditText) findViewById(R.id.editTextPasswordRegister)).getText().toString();
         fireUser = new FireUser();
-        fireUser.doLogin(email, password, LoginActivity.this, new CallbackInterface() {
+        fireUser.doRegister(email, pucpCode, password, RegisterActivity.this, new CallbackInterface() {
             @Override
             public void onComplete(Object result) {
                 dtoMessage = (DtoMessage) result;
@@ -47,19 +46,17 @@ public class LoginActivity extends AppCompatActivity {
                 switch (status) {
                     case 1:
                         // Redirigir el acceso a su respectiva pagina
-                        //startActivity(new Intent(LoginActivity.this, IncidenceActivity.class));
+                        //startActivity(new Intent(RegisterActivity.this, IncidenceActivity.class));
                         break;
                     case 2:
                         // Mostrar mensaje de error.
                         break;
                     default:
-                        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
                         break;
+
                 }
             }
         });
-
-
     }
-
 }
--- app/src/main/java/pucp/telecom/moviles/incidencias/webRequests/FireUser.java
+++ app/src/main/java/pucp/telecom/moviles/incidencias/webRequests/FireUser.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import pucp.telecom.moviles.incidencias.CallbackInterface;
@@ -19,6 +22,10 @@ public class FireUser {
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
 
+    /*
+     * 1: Ingreso exitoso.
+     * 0: Error en el ingreso. (FirebaseAuthInvalidUserException, FirebaseAuthInvalidCredentialsException)
+     * */
     public void doLogin(String email, String password, Activity context, final CallbackInterface callback) {
         mAuth = FirebaseAuth.getInstance();
         mAuth.signInWithEmailAndPassword(email, password)
@@ -32,6 +39,43 @@ public class FireUser {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         callback.onComplete(new DtoMessage("Error en el ingreso", 0));
+                    }
+                });
+
+    }
+
+    /*
+     * 1: Usuario registrado.
+     * 0: Error en el registro. (FirebaseAuthUserCollisionException, FirebaseAuthWeakPasswordException, FirebaseAuthInvalidCredentialsException)
+     * */
+    public void doRegister(String email, final String pucpCode, String password, Activity context, final CallbackInterface callback) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(context, new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        // Actualizar al usuario con su codigo pucp y su rol.
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(pucpCode + "-U")
+                                .build();
+                        user.updateProfile(profileUpdates)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            callback.onComplete(new DtoMessage("Usuario registrado", 1));
+                                        }
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(context, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+
+                        callback.onComplete(new DtoMessage("Error en el registro", 0));
                     }
                 });
 
